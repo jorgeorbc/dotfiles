f37ca57fdb07ed42ebbf04ab93b905721aebe449
Jorge Rubio <213683210+jorgeorbc@users.noreply.github.com>
2025-07-16 18:01:59 -0600

    Add qtile, neofetch and kitty config

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+volumeicon &
+setxkbmap -layout latam &
+picom &
+udiskie -t &
+nm-applet &
+dunst &
+echo 'xset r rate 200 30'
+# cbatticon &
---
+++ .config/qtile/myColors.py
@@ -0,0 +1,27 @@
+Gruvbox = {
+    "dark": ["#141819"],     # Fondo oscuro
+    "light": ["#ebdbb2"],    # Color base claro
+    "gray": ["#a89984"],     # Gris neutro
+    "red": "#cc241d",        # Rojo
+    "green": "#98971a",      # Verde
+    "yellow": "#d79921",     # Amarillo
+    "blue": "#458588",       # Azul
+    "magenta": "#b16286",    # Magenta
+    "cyan": "#689d6a",       # Cyan
+    "white": "#fbf1c7",      # Blanco cálido
+    "orange": "#d65d0e",     # Naranja
+}
+
+Kanagawa = {
+    "dark": ["#0d0c0c"],     # Fondo oscuro
+    "light": ["#c5c9c5"],    # Color base claro
+    "gray": ["#a6a69c"],     # Gris neutro
+    "red": "#DE6573",        # Rojo
+    "green": "#98BB6C",      # Verde
+    "yellow": "#C0A36E",     # Amarillo
+    "blue": "#7E9CD8",       # Azul
+    "magenta": "#D27E99",    # Magenta
+    "cyan": "#7E9CD8",       # Cyan
+    "white": "#c5c9c5",      # Blanco cálido
+    "orange": "#FF9E3B",     # Naranja
+}
